af6683a38dc34996b2304687ce5a92eabd29b663
Ded Laptop <GAMALSH1K@yandex.com>
2023-04-01 15:31:37 +0600

    Second commit

---
+++ WpfDecoration/WpfDecoration/Instance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDecoration
+{
+    internal class Instance
+    {
+        private static DecorationEntities _db;
+        public static DecorationEntities db
+        {
+            get
+            {
+                if(_db == null)
+                    _db = new DecorationEntities();
+                return _db;
+            }
+        }
+    }
+}
